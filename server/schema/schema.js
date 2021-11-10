9c8b0f06202981bd01aa64e9391cbffd5796f741
Lu, Bin <blu@wm.com>
2021-11-10 17:29:54 -0600

    Added post type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,31 @@
 const graphql = require("graphql");
+const _ = require('lodash')
 
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt,GraphQLSchema } = graphql;
 
+//Dummy data
+const users=[
+  {id:"11",name:"name 11", age:11,profession:"profession 11"},
+  {id:"1",name:"name 1", age:1,profession:"profession 1"},
+  {id:"111",name:"name 111", age:111,profession:"profession 111"},
+  {id:"1111",name:"name 1111", age:1111,profession:"profession 1111"}
+];
+
+//Dummy Hobby data
+const hobbies=[
+  {id:"1", title:"Hobby 1",description:"hobby dews 1"},
+  {id:"2", title:"Hobby 2",description:"hobby dews 2"},
+  {id:"3", title:"Hobby 3",description:"hobby dews 3"},
+  {id:"4", title:"Hobby 4",description:"hobby dews 4"}
+]
+
+//Post dummy data
+const posts=[
+  {id:"1",comment:"post comment 1"},
+  {id:"2",comment:"post comment 2"},
+  {id:"3",comment:"post comment 3"},
+  {id:"4",comment:"post comment 4"},
+];
 //Types
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -10,7 +34,28 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    profession:{type: GraphQLString}
   }),
+});
+
+//Hobby type
+const HobbyType = new GraphQLObjectType({
+  name:"Hobby",
+  description:"Hobby type",
+  fields:()=>({
+    id:{type:GraphQLID},
+    title:{type:GraphQLString},
+    description:{type:GraphQLString}
+  })
+});
+//Post type
+const PostType = new GraphQLObjectType({
+  name:"Post",
+  description:"Post type",
+  fields:()=>({
+    id:{type:GraphQLID},
+    comment:{type:GraphQLString}
+  })
 });
 
 //Root query
@@ -22,11 +67,47 @@ const RootQuery = new GraphQLObjectType({
       type:UserType,
       args:{id:{type:GraphQLString}},
       resolve(parent,args){
-        //Resolve data haere
+        return _.find(users,{id:args.id});
+      }
+    },
+    hobby:{
+      type:HobbyType,
+      args:{id:{type:GraphQLID}},
+      resolve(parent,args){
+        return _.find(hobbies,{id:args.id});
+      }
+    },
+    post:{
+      type:PostType,
+      args:{id:{type:GraphQLID}},
+      resolve(parent,args){
+        return _.find(posts,{id:args.id});
       }
     }
   }
 });
+
+
+/*Sample graphQL
+{
+  user(id:"1"){
+  id
+  name
+  age
+}
+,
+hobby(id:"1"){
+  id
+  title
+  description
+},
+post(id:"1"){
+  id
+  comment
+}
+}
+*/
+
 
 module.exports = new GraphQLSchema({
   query:RootQuery
